186bc9d89be6f107e2df37e249ca75ca65eafef3
mhdalihoria <alihoria123456@gmail.com>
2023-12-16 13:22:34 +0200

    using MUI's "useMediaQuery" to change font size based on screensize

--- components/sections/hero/Hero.jsx
+++ components/sections/hero/Hero.jsx
@@ -3,11 +3,12 @@ import styles from "./Hero.module.css";
 import Image from "next/image";
 import heroBg from "@/public/imgs/hero-bg.png";
 import svg from "@/public/imgs/thai-clothes.svg";
-import { Button, useTheme, Typography } from "@mui/material";
+import { Button, useTheme, Typography, useMediaQuery } from "@mui/material";
 
 const Hero = () => {
   const theme = useTheme();
   console.log(theme.palette.primary.main);
+  const mdBreakPoint = useMediaQuery(theme.breakpoints.up("md"));
   return (
     <section className={styles.heroContainer}>
       <section className={styles.heroBgContainer}>
@@ -26,7 +27,7 @@ const Hero = () => {
           align="center"
           color="text.primary"
           gutterBottom
-          className={styles.heroContentTitle}
+          sx={{ fontSize: mdBreakPoint ? "3.4rem" : "2rem" }}
         >
           Ready To Become The Champion You Always Dreamed Of?
         </Typography>
@@ -36,7 +37,8 @@ const Hero = () => {
           align="center"
           color="text.secondary"
           gutterBottom
-          className={styles.heroContentParagraph}
+          sx={{ fontSize: mdBreakPoint ? "1.2rem" : "1rem" }}
+
         >
           Kick-Start Your Martial Arts Journey To Learn Cool And Practical
           Disciplines
