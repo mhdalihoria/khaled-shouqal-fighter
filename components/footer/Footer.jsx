e2c0d6223c707f75a80f02db5581ecf857291ada
mhdalihoria <alihoria123456@gmail.com>
2023-12-16 13:37:26 +0200

    using useMediaQuery to Adjust Styles Accordingly

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Grid, useTheme, Button } from "@mui/material";
+import { Grid, useTheme, Button, useMediaQuery } from "@mui/material";
 import styles from "./Footer.module.css";
 import Image from "next/image";
 import Logo from "@/public/imgs/light-logo.svg";
 
 const Footer = () => {
   const theme = useTheme();
+  const mdBreakPoint = useMediaQuery(theme.breakpoints.up("md"));
 
   return (
     <footer
@@ -21,28 +22,47 @@ const Footer = () => {
         alignItems={"center"}
         sx={{ width: "80%", margin: "0 auto", paddingTop: "1rem" }}
       >
-        <Grid item>
-          <Image
-            src={Logo}
-            alt="Khaled-Shouqal-Logo"
-            width={200}
-            height={100}
-          />
+        <Grid item xs={12} sm={6}>
+          <div
+            className={
+              !mdBreakPoint
+                ? styles.centerGridItem
+                : styles.spacebetweenGridItem
+            }
+          >
+            <Image
+              src={Logo}
+              alt="Khaled-Shouqal-Logo"
+              width={200}
+              height={100}
+            />
+          </div>
         </Grid>
-        <Grid item>
-          <Button variant="text">
-            <i className="fa-brands fa-square-instagram" />
-          </Button>
-          <Button variant="text">
-            <i className="fa-brands fa-square-facebook" />
-          </Button>
-          <Button variant="text">
-            <i className="fa-brands fa-tiktok" />
-          </Button>
+        <Grid item xs={12} sm={6}>
+          <div
+            className={
+              !mdBreakPoint ? styles.centerGridItem : styles.flexEndGridItem
+            }
+          >
+            <Button variant="text">
+              <i className="fa-brands fa-square-instagram" />
+            </Button>
+            <Button variant="text">
+              <i className="fa-brands fa-square-facebook" />
+            </Button>
+            <Button variant="text">
+              <i className="fa-brands fa-tiktok" />
+            </Button>
+          </div>
         </Grid>
       </Grid>
       <div
-        style={{ textAlign: "center", paddingBottom: "1em", fontSize: ".8rem" }}
+        style={{
+          textAlign: "center",
+          paddingBottom: "1em",
+          fontSize: ".8rem",
+          marginTop: "1rem",
+        }}
       >
         © 2023 Khaled Shouqual Fighter, All Rights Reserved.
       </div>
