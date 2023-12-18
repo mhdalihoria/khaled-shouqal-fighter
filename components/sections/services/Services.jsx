5473b3b056ebb3e27f37351aaf13e4fb7f5a809e
mhdalihoria <alihoria123456@gmail.com>
2023-12-18 21:36:07 +0200

    - Adding Animation when component is In View using "useInView"
    - Code Refactoring

--- components/sections/services/Services.jsx
+++ components/sections/services/Services.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTheme, Grid } from "@mui/material";
 import styles from "./Services.module.css";
 import Punch from "@/public/imgs/thai-punch.svg";
@@ -6,18 +6,63 @@ import Elbow from "@/public/imgs/thai-elbow.svg";
 import Clothes from "@/public/imgs/thai-clothes.svg";
 import Kick from "@/public/imgs/thai-kick.svg";
 import Image from "next/image";
+import { useInView, animated } from "@react-spring/web";
 
 const Services = () => {
   const theme = useTheme();
+  const [ref, springs] = useInView(() => ({
+    from: {
+      opacity: 0,
+      y: 100,
+    },
+    to: {
+      opacity: 1,
+      y: 0,
+    },
+  }));
 
   const serviceTitleStyled = {
     color: theme.palette.text.secondary,
     marginTop: "1rem",
   };
+  
+  const servicesArr = [
+    {
+      title: "Learn To Punch Hard",
+      src: Punch,
+      alt: "muay thai punch",
+    },
+    {
+      title: "Throw Perfect Elbows",
+      src: Elbow,
+      alt: "muay thai elbow",
+    },
+    {
+      title: "Throw Most Brutal Kicks",
+      src: Kick,
+      alt: "muay thai kick",
+    },
+    {
+      title: "Get Traditional Clothes",
+      src: Clothes,
+      alt: "muay thai clothes",
+    },
+  ];
+
+  const servicesElements = servicesArr.map((service) => (
+    <Grid item xs={12} sm={6} md={3} lg={3} key={"title"}>
+      <animated.div className={styles.servicesItem} ref={ref} style={springs}>
+        <Image src={service.src} alt={service.alt} width={100} height={100} />
+        <p style={serviceTitleStyled}>{service.title}</p>
+      </animated.div>
+    </Grid>
+  ));
 
   return (
     <section
-      style={{ background: theme.palette.background.default }}
+      style={{
+        background: theme.palette.background.default,
+      }}
       className={styles.servicesSectionContainer}
     >
       <Grid
@@ -26,36 +71,7 @@ const Services = () => {
         alignItems="center"
         className={styles.servicesContainer}
       >
-        <Grid item xs={12} sm={6} md={3} lg={3}>
-          <div className={styles.servicesItem}>
-            <Image src={Punch} alt="muay thai punch" width={100} height={100} />
-            <p style={serviceTitleStyled}>Learn To Punch Hard</p>
-          </div>
-        </Grid>
-
-        <Grid item xs={12} sm={6} md={3} lg={3}>
-          <div className={styles.servicesItem}>
-            <Image src={Elbow} alt="muay thai elbow" width={100} height={100} />
-            <p style={serviceTitleStyled}>Throw Perfect Elbows</p>
-          </div>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3} lg={3}>
-          <div className={styles.servicesItem}>
-            <Image src={Kick} alt="muay thai kick" width={100} height={100} />
-            <p style={serviceTitleStyled}>Throw Most Brutal Kicks </p>
-          </div>
-        </Grid>
-        <Grid item xs={12} sm={6} md={3} lg={3}>
-          <div className={styles.servicesItem}>
-            <Image
-              src={Clothes}
-              alt="muay thai clothes"
-              width={100}
-              height={100}
-            />
-            <p style={serviceTitleStyled}>Get Traditional Clothes </p>
-          </div>
-        </Grid>
+        {servicesElements}
       </Grid>
     </section>
   );
